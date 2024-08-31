3c1930f98e43f6473c0898f5ac1510f0d6e97fc2
Maxson Almeida <maxsonferovante@gmail.com>
2024-08-31 19:11:34 -0300

    feat: Update main.py to include host configuration for uvicorn.run

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from src.config import configs
 
 if __name__ == '__main__':
     uvicorn.run(
-        'src.app_module:http_server',        
+        'src.app_module:http_server',
+        host="0.0.0.0",
         port=configs['port'],
         reload=True
     )
