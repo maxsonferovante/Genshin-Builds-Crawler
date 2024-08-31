b5e0bca3c01397097d0540d342e2b4b8e3043c15
Maxson Almeida <maxsonferovante@gmail.com>
2024-08-31 19:11:30 -0300

    feat: Add script to start server with PM2

---
+++ start_server.sh.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Criar um ambiente virtual
+python3 -m venv venv
+
+# Ativar o ambiente virtual
+. venv/bin/activate
+
+# Instalar as dependências do arquivo requirements.txt
+pip install -r requirements.txt
+
+# Iniciar o servidor usando PM2 com o nome "Genshin Builder Crawler API"
+pm2 start uvicorn --name "Genshin Builder Crawler API" -- "src.app_module:http_server" --host "0.0.0.0" --port "8000"
