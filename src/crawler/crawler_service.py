7af3948072594d1c17bacadaec18427bf302f42e
Maxson Almeida <maxsonferovante@gmail.com>
2024-09-03 20:31:57 -0300

    feat: Update crawler_service.py to improve weapon and character data retrieval

--- src/crawler/crawler_service.py
+++ src/crawler/crawler_service.py
@@ -1,11 +1,12 @@
 from nest.core import Injectable
 from src.config import configs
-
 from .crawler_model import EnumCrawler, CrawlerResponse, Item
 
+from datetime import datetime
 from urllib.parse import urljoin
 from bs4 import BeautifulSoup
 import requests
+
 
 @Injectable
 class CrawlerService:
@@ -16,47 +17,18 @@ class CrawlerService:
         
         self.__responseHTML = None
         self.__objectHTML = None
-
-    def run(self, option: EnumCrawler) -> CrawlerResponse:
+        
+        self.__last_datetime_updated = None
+    
+    
+    def get_weapon(self,option: EnumCrawler) -> CrawlerResponse:
+        
         self.__download_url()
         self.__get_information_response_html()
         
-        if option == EnumCrawler.WEAPON:
-        
-            result = self.__get_dict_weapon()
-            result.option = option
-            return result
-        
-        elif option == EnumCrawler.CHARACTER:
-            
-            result = self.__get_dict_character()
-            result.option = option
-            return result
-            
-        else:
-            raise ValueError('Option not found')
-            
-    
-    def __download_url(self):
-        try:
-            self.__responseHTML = requests.get(self.url_base, timeout=30).text       
-        
-        except requests.RequestException as e:
-            print(f'Error: {e}')
-            raise e
-
-    def __get_information_response_html(self):
-        self.__objectHTML = BeautifulSoup(self.__responseHTML, 'html.parser')
-
-
-    def __get_dict_dungeon(self):
-
-        for tr_item in self.__objectHTML.find_all('tr', class_='border-b border-gray-700 pt-2 align-middle'):
-            self.dictWeapon[tr_item.td.h3.text] = []
-
-
-    def __get_dict_weapon(self) -> CrawlerResponse:
         objectResponse = CrawlerResponse()
+        
+        objectResponse.option = option
         
         for td_item in self.__objectHTML.find_all('tr', class_='border-b border-gray-700 pt-2 align-middle'):
 
@@ -74,8 +46,14 @@ class CrawlerService:
 
         return objectResponse
     
-    def __get_dict_character(self) -> CrawlerResponse:
+    def get_character(self, option: EnumCrawler) -> CrawlerResponse:
+        
+        self.__download_url()
+        self.__get_information_response_html()
+        
         objectResponse = CrawlerResponse()
+        
+        objectResponse.option = option
         
         weapon = self.__objectHTML.find_all('tr', class_='border-b border-gray-700 pt-2 align-middle')
         
@@ -96,6 +74,48 @@ class CrawlerService:
         
         return objectResponse
 
+    def __download_url(self):
+        try:
+            if self.__available_update():
+                self.__responseHTML = requests.get(self.url_base, timeout=30).text  
+                self.__updated_last_datetime()
+            else:
+                print('No update available')
+                
+                                     
+        except requests.RequestException as e:
+            print(f'Error: {e}')
+            raise e
+
+    def __get_information_response_html(self):
+        self.__objectHTML = BeautifulSoup(self.__responseHTML, 'html.parser')
+
+    def __available_update(self):
+        
+        if self.__last_datetime_updated is None:
+            return True
+        
+        datetime_now = datetime.now()
+        difference = (datetime.now() - self.__last_datetime_updated).seconds
+        
+        print (f'Datetime now: {datetime_now} - Last update: {self.__last_datetime_updated} - Difference: {difference}')
+    
+        
+        if difference > 60:
+            print('Update available')
+            return True
+        print ('Update not available')
+        return False
+    
+    def __updated_last_datetime(self):
+        self.__last_datetime_updated = datetime.now()
+        print (f'Last update: {self.__last_datetime_updated}')
+    
+    def __get_dict_dungeon(self):
+
+        for tr_item in self.__objectHTML.find_all('tr', class_='border-b border-gray-700 pt-2 align-middle'):
+            self.dictWeapon[tr_item.td.h3.text] = []
+   
     def __get_img_weapon(self,path):
         # pt/weapon/harbinger_of_dawn
         # https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/weapons/harbinger_of_dawn.png?strip=all&quality=100&w=80
