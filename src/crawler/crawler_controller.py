966d79edabfc1718bb6d60290fac9fa3f1ef1d61
Maxson Almeida <maxsonferovante@gmail.com>
2024-09-03 21:48:47 -0300

    feat: Add support for retrieving crawler weapons, characters, dungeons, and teams

--- src/crawler/crawler_controller.py
+++ src/crawler/crawler_controller.py
@@ -1,6 +1,6 @@
 from nest.core import Controller, Get, Post
 from .crawler_service import CrawlerService
-from .crawler_model import EnumCrawler
+from .crawler_model import EnumCrawler, WeaponsResponse, CharacterResponse, DungeonsResponse, TeamsResponse
 
 
 @Controller("crawler")
@@ -10,17 +10,17 @@ class CrawlerController:
         self.crawler_service = crawler_service
     
     @Get("/weapons")
-    def get_crawler_weapons(self):
+    def get_crawler_weapons(self) -> WeaponsResponse:
         return self.crawler_service.get_weapon(EnumCrawler.WEAPON)
     
     @Get("/characters")
-    def get_crawler_characters(self):
+    def get_crawler_characters(self) -> CharacterResponse:
         return self.crawler_service.get_character(EnumCrawler.CHARACTER)
         
     @Get("/dungeons")
-    def get_crawler_dungeons(self):
+    def get_crawler_dungeons(self) -> DungeonsResponse:
         return self.crawler_service.get_dungeon()
     
     @Get("/teams")
-    def get_crawler_teams(self):
+    def get_crawler_teams(self) -> TeamsResponse:
         return self.crawler_service.get_teams()
