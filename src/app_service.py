af253851ceaf3455910de46b9310e8030e47d4b6
Maxson Almeida <maxsonferovante@gmail.com>
2024-09-12 00:54:12 -0300

    feat: Atualizar versão para 0.0.4 em app_service.py

--- src/app_service.py
+++ src/app_service.py
@@ -6,7 +6,7 @@ from nest.core import Injectable
 class AppService:
     def __init__(self):
         self.app_name = "Genshin Builds Crawler API"
-        self.app_version = "0.0.3"
+        self.app_version = "0.0.4"
 
     def get_app_info(self):
         return {"app_name": self.app_name, "app_version": self.app_version}
